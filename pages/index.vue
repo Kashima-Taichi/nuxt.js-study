20ad363f67a80df9e2f75ba833db222fcfc32a77
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-08-28 22:56:22 +0900

    3-29 propsの設定

--- pages/index.vue
+++ pages/index.vue
@@ -4,27 +4,22 @@
       <h1>Get the latest tech news!</h1>
     </section>
     <section class="feature-posts">
-      <nuxt-link :to="'/posts/' + 1">
-        <article class="post-preview">
-          <div class="post-thumbnail"></div>
-          <div class="post-content">
-            <h1>Post Title</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link :to="'/posts/' + 2">
-        <article class="post-preview">
-          <div class="post-thumbnail"></div>
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-            <p>Preview Text 2</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview id="1" thumbnail title="Hello there !" previewText="This is my first post!" />
+      <PostPreview id="2" thumbnail title="Hello there !!" previewText="This is my first post!" />
+      <PostPreview id="3" thumbnail title="Hello there !!!" previewText="This is my first post!" />
     </section>
   </div>
 </template>
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+
+export default {
+  components: {
+    PostPreview,
+  },
+};
+</script>
 
 <style scoped>
 .intro {
@@ -64,41 +59,5 @@
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>
